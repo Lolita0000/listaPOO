a4e1a0bf68b358ad415cfd52b0f7e46da978609b
Ana Luiza Rodrigues Machado <134735637+Lolita0000@users.noreply.github.com>
2024-09-11 15:54:37 -0300

    Update aluno.java

--- poo/aluno.java
+++ poo/aluno.java
@@ -14,7 +14,7 @@ package br.com.notas;
 	    private String atribuirConceito(double nota) {
 	        if (nota >= 0 && nota < 5.0) {
 	            return "D";
-	        } else if (nota >= 6.0 && nota < 7.0) {
+	        } else if (nota >= 5.0 && nota < 7.0) {
 	            return "C";
 	        } else if (nota >= 7.0 && nota < 9.0) {
 	            return "B";
